1e6edab8fc4ab65ab4035952c5d05f3d18cdcb65
samikun <samikun@vt.edu>
2023-08-04 19:04:24 -0400

    Adding matplotlib to kivy app (simple but large graph). Kivy Resources added to README file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # GPU_Based_GUI
+
+Some Resources:
+ - Using matplotlib with kivy: https://www.youtube.com/watch?v=83C4tl8scoY
+ - Installing kivy: https://kivy.org/doc/stable/gettingstarted/installation.html
--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
+#from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
+import matplotlib.pyplot as plt
 
-class Test(Widget):
-    pass
+x = []
+y = []
+
+for i in range(1000000):
+    x.append(i)
+    y.append(i * i)
+
+plt.plot(x, y)
+plt.ylabel("Y Axis")
+plt.xlabel("X Axis")
+
+class Test(FloatLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        box = self.ids.box
+        box.add_widget(FigureCanvasKivyAgg(plt.gcf()))
 
 
 class TestApp(App):
